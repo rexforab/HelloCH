0345aa7b094417dad0780c3ba29483f5b624aac4
Alex <rexforab@gmail.com>
2016-11-06 16:07:49 -0500

    fix references

--- HelloWorld/tests/HelloWorld.UnitTests/HelloControllerTests.cs
+++ HelloWorld/tests/HelloWorld.UnitTests/HelloControllerTests.cs
@@ -2,6 +2,7 @@
 using HelloWorld.Controllers;
 using HelloService.Interfaces;
 using Moq;
+using HelloEntities;
 
 namespace HelloWorld.UnitTests
 {
@@ -14,7 +15,7 @@ namespace HelloWorld.UnitTests
 
             // Arrange
             var mockService = new Mock<IMessageService>();
-            mockService.Setup(repo => repo.GetHello()).Returns("Test Success");
+            mockService.Setup(repo => repo.GetMessage(It.IsAny<int>())).Returns(new Message() { HelloMessage = "Test Success" });
 
             var controller = new HelloController(mockService.Object);
 
